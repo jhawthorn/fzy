306443765cce8dfe9a81187fc100ae4d447660af
John Hawthorn <john.hawthorn@gmail.com>
2014-07-12 15:22:44 -0700

    Add some special cases

--- match.c
+++ match.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <string.h>
 
 static int is_subset(const char *needle, const char *haystack){
 	while(*needle){
@@ -11,8 +12,13 @@ static int is_subset(const char *needle, const char *haystack){
 }
 
 double match(const char *needle, const char *haystack){
-	if(!is_subset(needle, haystack)){
+	if(!*needle){
+		return 1.0;
+	}else if(!is_subset(needle, haystack)){
 		return 0.0;
+	}else if(!strcasecmp(needle, haystack)){
+		return 1.0;
+	}else{
+		return 0.9;
 	}
-	return 1.0;
 }
