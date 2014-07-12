48c627b0603fe2bf061f520dae094afc0b1c3943
John Hawthorn <john.hawthorn@gmail.com>
2014-07-12 15:18:56 -0700

    Refactor into is_subset function

--- match.c
+++ match.c
@@ -1,12 +1,18 @@
 #include <ctype.h>
 
-double match(const char *needle, const char *haystack){
+static int is_subset(const char *needle, const char *haystack){
 	while(*needle){
 		if(!*haystack)
-			return 0.0;
-		while(*haystack && tolower(*needle) == tolower(*haystack++)){
+			return 0;
+		while(*haystack && tolower(*needle) == tolower(*haystack++))
 			needle++;
-		}
+	}
+	return 1;
+}
+
+double match(const char *needle, const char *haystack){
+	if(!is_subset(needle, haystack)){
+		return 0.0;
 	}
 	return 1.0;
 }
