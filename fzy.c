c2b76921508fa2ab627e25b8a95648be67e05f88
John Hawthorn <john.hawthorn@gmail.com>
2014-07-26 23:42:02 -0700

    Don't break ties

--- fzy.c
+++ fzy.c
@@ -64,8 +64,7 @@ static int cmpchoice(const void *p1, const void *p2) {
 	double score2 = choices_score[idx2];
 
 	if(score1 == score2)
-		/* break ties by length of result */
-		return strlen(choices[idx1]) - strlen(choices[idx2]);
+		return 0;
 	else if(score1 < score2)
 		return 1;
 	else
