00e8069b4e228af1bf3e2ae282f368676efbe0db
John Hawthorn <john.hawthorn@gmail.com>
2014-09-18 17:28:21 -0700

    Move scoring constants into config.h

--- config.h
+++ config.h
@@ -1,1 +1,10 @@
 #define TTY_COLOR_HIGHLIGHT TTY_COLOR_YELLOW
+
+#define SCORE_GAP_LEADING      -0.005
+#define SCORE_GAP_TRAILING     -0.005
+#define SCORE_GAP_INNER        -0.01
+#define SCORE_MATCH_CONSECUTIVE 1.0
+#define SCORE_MATCH_SLASH       0.9
+#define SCORE_MATCH_WORD        0.8
+#define SCORE_MATCH_CAPITAL     0.7
+#define SCORE_MATCH_DOT         0.6
--- match.c
+++ match.c
@@ -7,6 +7,8 @@
 
 #include "match.h"
 
+#include "config.h"
+
 int has_match(const char *needle, const char *haystack){
 	while(*needle){
 		char nch = tolower(*needle++);
@@ -74,15 +76,6 @@ double calculate_score(const char *needle, const char *haystack, size_t *positio
 	 * D[][] Stores the best score for this position ending with a match.
 	 * M[][] Stores the best possible score at this position.
 	 */
-
-#define SCORE_GAP_LEADING      -0.005
-#define SCORE_GAP_TRAILING     -0.005
-#define SCORE_GAP_INNER        -0.01
-#define SCORE_MATCH_CONSECUTIVE 1.0
-#define SCORE_MATCH_SLASH       0.9
-#define SCORE_MATCH_WORD        0.8
-#define SCORE_MATCH_CAPITAL     0.7
-#define SCORE_MATCH_DOT         0.6
 
 	/* Which positions are beginning of words */
 	char last_ch = '\0';
