c373470cbb72af3f1432f01c255c7ce69eb04aee
John Hawthorn <john.hawthorn@gmail.com>
2016-12-30 16:25:54 -0800

    Upgrade to ttytest 0.4.0

--- test/integration/integration_test.rb
+++ test/integration/integration_test.rb
@@ -13,83 +13,96 @@ class FzyTest < Minitest::Test
 
   def test_empty_list
     @tty = TTYtest.new_terminal(%{echo placeholder;echo -n "" | #{FZY_PATH}})
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '>')
-    @tty.assert_row(2, '')
     @tty.assert_cursor_position(y: 1, x: 2)
+    @tty.assert_matches <<~TTY
+      placeholder
+      >
+    TTY
 
     @tty.send_keys('t')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> t')
-    @tty.assert_row(2, '')
     @tty.assert_cursor_position(y: 1, x: 3)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > t
+    TTY
 
     @tty.send_keys('z')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> tz')
-    @tty.assert_row(2, '')
     @tty.assert_cursor_position(y: 1, x: 4)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > tz
+    TTY
 
     @tty.send_keys("\r")
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, 'tz')
     @tty.assert_cursor_position(y: 2, x: 0)
+    @tty.assert_matches <<~TTY
+      placeholder
+      tz
+    TTY
   end
 
   def test_one_item
     @tty = TTYtest.new_terminal(%{echo placeholder;echo -n "test" | #{FZY_PATH}})
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '>')
-    @tty.assert_row(2, 'test')
-    @tty.assert_row(3, '')
+    @tty.assert_matches <<~TTY
+      placeholder
+      >
+      test
+    TTY
     @tty.assert_cursor_position(y: 1, x: 2)
 
     @tty.send_keys('t')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> t')
-    @tty.assert_row(2, 'test')
-    @tty.assert_row(3, '')
     @tty.assert_cursor_position(y: 1, x: 3)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > t
+      test
+    TTY
 
     @tty.send_keys('z')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> tz')
-    @tty.assert_row(2, '')
-    @tty.assert_row(3, '')
     @tty.assert_cursor_position(y: 1, x: 4)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > tz
+    TTY
 
     @tty.send_keys("\r")
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, 'tz')
     @tty.assert_cursor_position(y: 2, x: 0)
+    @tty.assert_matches <<~TTY
+      placeholder
+      tz
+    TTY
   end
 
   def test_two_items
     @tty = TTYtest.new_terminal(%{echo placeholder;echo -n "test\nfoo" | #{FZY_PATH}})
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '>')
-    @tty.assert_row(2, 'test')
-    @tty.assert_row(3, 'foo')
-    @tty.assert_row(4, '')
     @tty.assert_cursor_position(y: 1, x: 2)
+    @tty.assert_matches <<~TTY
+      placeholder
+      >
+      test
+      foo
+    TTY
 
     @tty.send_keys('t')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> t')
-    @tty.assert_row(2, 'test')
-    @tty.assert_row(3, '')
     @tty.assert_cursor_position(y: 1, x: 3)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > t
+      test
+    TTY
 
     @tty.send_keys('z')
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> tz')
-    @tty.assert_row(2, '')
-    @tty.assert_row(3, '')
     @tty.assert_cursor_position(y: 1, x: 4)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > tz
+    TTY
 
     @tty.send_keys("\r")
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, 'tz')
+    @tty.assert_matches <<~TTY
+      placeholder
+      tz
+    TTY
     @tty.assert_cursor_position(y: 2, x: 0)
   end
 
@@ -99,56 +112,66 @@ class FzyTest < Minitest::Test
 
   def test_editing
     @tty = TTYtest.new_terminal(%{echo placeholder;echo -n "test\nfoo" | #{FZY_PATH}})
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '>')
     @tty.assert_cursor_position(y: 1, x: 2)
+    @tty.assert_matches <<~TTY
+      placeholder
+      >
+      test
+      foo
+    TTY
 
     @tty.send_keys("foo bar baz")
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> foo bar baz')
     @tty.assert_cursor_position(y: 1, x: 13)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > foo bar baz
+    TTY
 
     @tty.send_keys(ctrl('H'))
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> foo bar ba')
     @tty.assert_cursor_position(y: 1, x: 12)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > foo bar ba
+    TTY
 
     @tty.send_keys(ctrl('W'))
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '> foo bar')
     @tty.assert_cursor_position(y: 1, x: 10)
+    @tty.assert_matches <<~TTY
+      placeholder
+      > foo bar
+    TTY
 
     @tty.send_keys(ctrl('U'))
-    @tty.assert_row(0, 'placeholder')
-    @tty.assert_row(1, '>')
     @tty.assert_cursor_position(y: 1, x: 2)
+    @tty.assert_matches <<~TTY
+      placeholder
+      >
+      test
+      foo
+    TTY
   end
 
   def test_ctrl_d
     @tty = TTYtest.new_terminal(%{echo -n "foo\nbar" | #{FZY_PATH}})
-    @tty.assert_row(0, '>')
+    @tty.assert_matches ">\nfoo\nbar"
 
     @tty.send_keys('foo')
-    @tty.assert_row(0, '> foo')
+    @tty.assert_matches "> foo\nfoo"
 
     @tty.send_keys(ctrl('D'))
-    @tty.assert_row(0, '')
-    @tty.assert_row(1, '')
-    @tty.assert_row(2, '')
+    @tty.assert_matches ''
     @tty.assert_cursor_position(y: 0, x: 0)
   end
 
   def test_ctrl_c
     @tty = TTYtest.new_terminal(%{echo -n "foo\nbar" | #{FZY_PATH}})
-    @tty.assert_row(0, '>')
+    @tty.assert_matches ">\nfoo\nbar"
 
     @tty.send_keys('foo')
-    @tty.assert_row(0, '> foo')
+    @tty.assert_matches "> foo\nfoo"
 
     @tty.send_keys(ctrl('C'))
-    @tty.assert_row(0, '')
-    @tty.assert_row(1, '')
-    @tty.assert_row(2, '')
+    @tty.assert_matches ''
     @tty.assert_cursor_position(y: 0, x: 0)
   end
 end
