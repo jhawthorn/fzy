115abca0bcd470bf3ea2d732fd19238c251fb6d1
leo-arch <leonardoabramovich2@gmail.com>
2022-04-18 03:38:09 -0300

    Fix compilation

--- src/config.def.h
+++ src/config.def.h
@@ -17,3 +17,34 @@
 #define DEFAULT_NUM_LINES 10
 #define DEFAULT_WORKERS 0
 #define DEFAULT_SHOW_INFO 0
+#define DEFAULT_DELIMITER '\n'
+#define DEFAULT_BENCHMARK 0
+#define DEFAULT_SCORES 0
+#define DEFAULT_SCROLLOFF 0
+#define DEFAULT_FILTER NULL
+#define DEFAULT_INIT_SEARCH NULL
+#define DEFAULT_MARKER '*'
+#define DEFAULT_POINTER '>'
+#define DEFAULT_PAD 0
+#define DEFAULT_MULTI 0
+#define DEFAULT_CYCLE 0
+#define DEFAULT_TAB_ACCEPTS 0
+#define DEFAULT_RIGHT_ACCEPTS 0
+#define DEFAULT_LEFT_ABORTS 0
+
+#define DEF_PROMPT_COLOR "\x1b[0;36m"
+#define DEF_POINTER_COLOR "\x1b[0;34m"
+#define DEF_MARKER_COLOR "\x1b[0;32m"
+#define DEF_SEL_FG_COLOR "\x1b[0m"
+#define DEF_SEL_BG_COLOR "\x1b[7m"
+#define NC "\x1b[0m" /* Reset attributes */
+
+/* Color indices: colors (from FZY_COLORS env var) will be parsed
+ * exactly in this order. See tty_interface.c */
+#define PROMPT_COLOR 0
+#define POINTER_COLOR 1
+#define MARKER_COLOR 2
+#define SEL_FG_COLOR 3
+#define SEL_BG_COLOR 4
+#define COLOR_ITEMS_NUM 5
+#define MAX_COLOR_LEN 48
