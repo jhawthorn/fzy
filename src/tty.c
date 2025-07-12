58755d60130286caf7129a56eb5f10ad9b059d83
John Hawthorn <john@hawthorn.email>
2025-07-11 21:06:17 -0700

    Increase tty buffer to 16k
    This should avoid tearing when displaying many very long results

--- src/tty.c
+++ src/tty.c
@@ -39,7 +39,7 @@ void tty_init(tty_t *tty, const char *tty_filename) {
 		exit(EXIT_FAILURE);
 	}
 
-	if (setvbuf(tty->fout, NULL, _IOFBF, 4096)) {
+	if (setvbuf(tty->fout, NULL, _IOFBF, 16384)) {
 		perror("setvbuf");
 		exit(EXIT_FAILURE);
 	}
